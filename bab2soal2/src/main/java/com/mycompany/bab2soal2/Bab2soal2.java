ab3ff43a5acae4499878bdd08ec7be0f9c0363f4
dzikriadjira <145428828+dzikriadjira@users.noreply.github.com>
2024-08-13 08:18:07 +0700

    Add files via upload

---
+++ bab2soal2/src/main/java/com/mycompany/bab2soal2/Bab2soal2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab2soal2 {
+
+    public static String makanan;
+        public static void main(String[] args)  {
+            makanan = "Ayam Geprek";
+        String minuman = "Jus Jambu";
+            System.out.println("Makanan Favorite saya : " + makanan) ;
+            System.out.println("Minuman Favorite Saya : " + minuman) ;
+        }
+    }
+    
+
