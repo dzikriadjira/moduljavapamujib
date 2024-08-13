e3f2d38c7c44ebffe6fa2bd2d19aab47fb6a1308
dzikriadjira <145428828+dzikriadjira@users.noreply.github.com>
2024-08-13 10:19:23 +0700

    Add files via upload

---
+++ bab9soal1/src/main/java/com/mycompany/bab9soal1/Bab9soal1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab9soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab9soal1 {
+
+    public static void main(String[] args) {
+       
+  
+        int i = 1; // Variabel untuk baris
+
+        while (i <= 5) {
+            int j = 1; // Variabel untuk kolom pada setiap baris
+            
+            while (j <= i) {
+                System.out.print("* ");
+                j++;
+            }
+
+            System.out.println(); // Baris baru
+            i++;
+        }
+    }
+}
